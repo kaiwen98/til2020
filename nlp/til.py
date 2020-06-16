b5ae0a560f9fad1d50deb18ab16806cbd9a9c653
kaiwen98 <e0406836@u.nus.edu>
2020-06-16 22:40:25 +0800

    ensemble

--- nlp/til.py
+++ nlp/til.py
@@ -4,6 +4,15 @@
 #Got it to PLB 0.984 with 10fold cv on local computer after playing with parameters
 #Try to improve score on your own local pc or throw it in the blender with the rest of them :)
 # %% [code] {"scrolled:true"}
+from __future__ import absolute_import, division
+import math
+import tensorflow as tf
+from keras.layers import Dense, Input, Embedding, Lambda, Dropout, Activation, SpatialDropout1D, Reshape, GlobalAveragePooling1D, Flatten, Bidirectional, CuDNNGRU, add, Conv1D, GlobalMaxPooling1D
+from keras import optimizers
+from keras import initializers
+from keras.layers import InputSpec, Layer
+from keras import backend as K
+import keras
 import os
 import gc
 import numpy as np
@@ -16,25 +25,50 @@ from keras import backend as K
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import roc_auc_score
 from sklearn.model_selection import KFold
-from keras.models import Model, model_from_json
+from keras.models import Model, model_from_json, load_model
 from keras.layers import Input, Dense, Embedding, MaxPooling1D, Conv1D, SpatialDropout1D
 from keras.layers import add, Dropout, PReLU, BatchNormalization, GlobalMaxPooling1D
 from keras.preprocessing import text, sequence
-from keras.callbacks import Callback
+from keras.callbacks import Callback, EarlyStopping, ModelCheckpoint
 from keras import optimizers
 from keras import initializers, regularizers, constraints, callbacks
 from sklearn.utils import shuffle
 from tensorflow.compat.v1.keras.optimizers import Adam, SGD
-from keras.backend import sigmoid
-from keras.utils.generic_utils import get_custom_objects
+from tensorflow.keras.backend import sigmoid
+from tensorflow.keras.utils import get_custom_objects
+import models 
 
 import tensorflow as tf
 
+
+
 EMBEDDING_FILE = './input/word_embeddings.pkl'
-#model_file = 'model_cv.json'
-model_file = 'model1.json'
-#weights_file = 'model_cv.h5'
-weights_file = 'model1.h5'
+
+def f1(y_pred0, y_test0):
+    TP = 0.0
+    FP = 0.0
+    FN = 0.0
+    TN = 0.0
+    y_pred = [item for sublist in y_pred0 for item in sublist]
+    y_test = [item for sublist in y_test0 for item in sublist]
+    print(y_pred)
+    print(y_test)
+    for i in range(len(y_pred)):
+        if y_pred[i] == 0: 
+            if y_test[i] == 0: 
+                TN += 1
+            else:
+                FN += 1
+        else:
+            if y_test[i] == 0: 
+                FP += 1
+            else: 
+                TP += 1
+    
+    print("FP: ", FP,"  TP: ", TP,"  FN: ", FN, " TN: ", TN)
+    precision = TP /(TP + FP)
+    recall = TP/(TP+FN)
+    return float(2 * precision * recall / (precision + recall))
 
 
 def swish(x, beta = 1): return (x * sigmoid(beta*x))
@@ -58,41 +92,14 @@ def extract_embed(fd):
     all_embs = np.stack(embeddings_index.values())
     return all_embs.mean(), all_embs.std(), embeddings_index
 
-def load_model():
-    # load json and create model
-    json_file = open(model_file, 'r')
-    loaded_model_json = json_file.read()
-    json_file.close()
-    model = model_from_json(loaded_model_json)
-    # load weights into new model
-    model.load_weights(weights_file)
-    print("Old model: ", type(model))
-    
-    model.compile(loss='binary_crossentropy', 
-            optimizer=optimizers.Adam(),
-            metrics=['accuracy'])
-            
-    print("Loaded model from disk")
-    return model
-
-def save_model(model):
-    model_json = model.to_json()
-    with open(model_file, "w") as json_file:
-        json_file.write(model_json)
-    # serialize weights to HDF5
-    model.save_weights(weights_file)
-    print("Saved model to disk")
-
 def schedule(ind):
-    a = [0.001, 0.0005, 0.0001, 0.0001]
+    a = [0.001, 0.0005, 0.0001, 0.0001, 0.00005, 0.00005]
     return a[ind] 
 
 #straightfoward preprocess
 
-
-
 # %% [code] {"scrolled:true"}
-def classifier(model, emb_mean, emb_std, embeddings_index):
+def classifier(model_name, emb_mean, emb_std, embeddings_index):
     train = pd.read_csv('./input/TIL_NLP_train1_dataset.csv')
     test = pd.read_csv('./input/TIL_NLP_unseen_dataset.csv')
     print('running classifier')
@@ -101,11 +108,13 @@ def classifier(model, emb_mean, emb_std, embeddings_index):
     print(max_features)
     maxlen = 200
     embed_size = 100
- 
+    train = shuffle(train)
+    test = shuffle(test)
     X_train = train["word_representation"].fillna("fillna").values
     y_train = train[["outwear", "top", "trousers", "women dresses", "women skirts"]].values
     X_test = test["word_representation"].fillna("fillna").values    
     y_test = test[["outwear", "top", "trousers", "women dresses", "women skirts"]].values
+    y_test = y_test.tolist()
     print('preprocessing start')
     tokenizer = text.Tokenizer(num_words=max_features)
     tokenizer.fit_on_texts(list(X_train) + list(X_test))
@@ -129,153 +138,32 @@ def classifier(model, emb_mean, emb_std, embeddings_index):
         
     print('preprocessing done')
 
-    # session_conf = tf.ConfigProto(intra_op_parallelism_threads=4, inter_op_parallelism_threads=4)
-    # K.set_session(tf.Session(graph=tf.get_default_graph(), config=session_conf))
+    model = models.DPCNN(maxlen, max_features, embed_size, embedding_matrix)
 
-    #model
-    #wrote out all the blocks instead of looping for simplicity
-
-
-   
-    filter_nr = 64
-    filter_size = 3
-    max_pool_size = 3
-    max_pool_strides = 2
-    dense_nr = 256
-    spatial_dropout = 0.2
-    dense_dropout = 0.5
-    train_embed = False
-    conv_kern_reg = regularizers.l2(0.00001)
-    conv_bias_reg = regularizers.l2(0.00001)
+    num_folds = 8
+    num = 0
+    kfold = KFold(n_splits=num_folds, shuffle=True)
     
-    comment = Input(shape=(maxlen,))
-    emb_comment = Embedding(max_features, embed_size, weights=[embedding_matrix], trainable=train_embed)(comment)
-    emb_comment = SpatialDropout1D(spatial_dropout)(emb_comment)
-
-    block1 = Conv1D(filter_nr, kernel_size=filter_size, padding='same', activation='linear', 
-                kernel_regularizer=conv_kern_reg, bias_regularizer=conv_bias_reg)(emb_comment)
-    block1 = BatchNormalization()(block1)
-    block1 = PReLU()(block1)
-    block1 = Conv1D(filter_nr, kernel_size=filter_size, padding='same', activation='linear', 
-                kernel_regularizer=conv_kern_reg, bias_regularizer=conv_bias_reg)(block1)
-    block1 = BatchNormalization()(block1)
-    block1 = PReLU()(block1)
-
-    #we pass embedded comment through conv1d with filter size 1 because it needs to have the same shape as block output
-    #if you choose filter_nr = embed_size (300 in this case) you don't have to do this part and can add emb_comment directly to block1_output
-    resize_emb = Conv1D(filter_nr, kernel_size=1, padding='same', activation='linear', 
-                kernel_regularizer=conv_kern_reg, bias_regularizer=conv_bias_reg)(emb_comment)
-    resize_emb = PReLU()(resize_emb)
+    for train, test in kfold.split(x_train, y_train):
+    
+        print("Training Fold number: ", num)
+        batch_size = 128
+        epochs = 6
+        lr = callbacks.LearningRateScheduler(schedule)
+        ra_val = RocAucEvaluation(validation_data=(x_train[test], y_train[test]), interval = 1)
+        es = EarlyStopping(monitor = 'val_loss', verbose = 1, patience = 2, restore_best_weights = True, mode = 'min')
+        mc = ModelCheckpoint(model_name, monitor='val_loss', mode='min', verbose=1, save_best_only= True, save_weights_only=True)
+        model.fit(x_train[train], y_train[train], batch_size=batch_size, epochs=epochs, validation_data=(x_train[test], y_train[test]), callbacks = [lr, ra_val, es, mc] ,verbose = 1)
+        num += 1
         
-    block1_output = add([block1, resize_emb])
-    block1_output = MaxPooling1D(pool_size=max_pool_size, strides=max_pool_strides)(block1_output)
-
-    block2 = Conv1D(filter_nr, kernel_size=filter_size, padding='same', activation='linear', 
-                kernel_regularizer=conv_kern_reg, bias_regularizer=conv_bias_reg)(block1_output)
-    block2 = BatchNormalization()(block2)
-    block2 = PReLU()(block2)
-    block2 = Conv1D(filter_nr, kernel_size=filter_size, padding='same', activation='linear', 
-                kernel_regularizer=conv_kern_reg, bias_regularizer=conv_bias_reg)(block2)
-    block2 = BatchNormalization()(block2)
-    block2 = PReLU()(block2)
+        y_pred = model.predict(x_test)
+        y_pred = [[1 if i > 0.5 else 0 for i in r] for r in y_pred]
         
-    block2_output = add([block2, block1_output])
-    block2_output = MaxPooling1D(pool_size=max_pool_size, strides=max_pool_strides)(block2_output)
-
-    block3 = Conv1D(filter_nr, kernel_size=filter_size, padding='same', activation='linear', 
-                kernel_regularizer=conv_kern_reg, bias_regularizer=conv_bias_reg)(block2_output)
-    block3 = BatchNormalization()(block3)
-    block3 = PReLU()(block3)
-    block3 = Conv1D(filter_nr, kernel_size=filter_size, padding='same', activation='linear', 
-                kernel_regularizer=conv_kern_reg, bias_regularizer=conv_bias_reg)(block3)
-    block3 = BatchNormalization()(block3)
-    block3 = PReLU()(block3)
-        
-    block3_output = add([block3, block2_output])
-    block3_output = MaxPooling1D(pool_size=max_pool_size, strides=max_pool_strides)(block3_output)
-
-    block4 = Conv1D(filter_nr, kernel_size=filter_size, padding='same', activation='linear', 
-                kernel_regularizer=conv_kern_reg, bias_regularizer=conv_bias_reg)(block3_output)
-    block4 = BatchNormalization()(block4)
-    block4 = PReLU()(block4)
-    block4 = Conv1D(filter_nr, kernel_size=filter_size, padding='same', activation='linear', 
-                kernel_regularizer=conv_kern_reg, bias_regularizer=conv_bias_reg)(block4)
-    block4 = BatchNormalization()(block4)
-    block4 = PReLU()(block4)
-
-    block4_output = add([block4, block3_output])
-    block4_output = MaxPooling1D(pool_size=max_pool_size, strides=max_pool_strides)(block4_output)
-
-    block5 = Conv1D(filter_nr, kernel_size=filter_size, padding='same', activation='linear', 
-                kernel_regularizer=conv_kern_reg, bias_regularizer=conv_bias_reg)(block4_output)
-    block5 = BatchNormalization()(block5)
-    block5 = PReLU()(block5)
-    block5 = Conv1D(filter_nr, kernel_size=filter_size, padding='same', activation='linear', 
-                kernel_regularizer=conv_kern_reg, bias_regularizer=conv_bias_reg)(block5)
-    block5 = BatchNormalization()(block5)
-    block5 = PReLU()(block5)
-
-    block5_output = add([block5, block4_output])
-    block5_output = MaxPooling1D(pool_size=max_pool_size, strides=max_pool_strides)(block5_output)
-
-    block6 = Conv1D(filter_nr, kernel_size=filter_size, padding='same', activation='linear', 
-                kernel_regularizer=conv_kern_reg, bias_regularizer=conv_bias_reg)(block5_output)
-    block6 = BatchNormalization()(block6)
-    block6 = PReLU()(block6)
-    block6 = Conv1D(filter_nr, kernel_size=filter_size, padding='same', activation='linear', 
-                kernel_regularizer=conv_kern_reg, bias_regularizer=conv_bias_reg)(block6)
-    block6 = BatchNormalization()(block6)
-    block6 = PReLU()(block6)
-
-    block6_output = add([block6, block5_output])
-    block6_output = MaxPooling1D(pool_size=max_pool_size, strides=max_pool_strides)(block6_output)
-
-    block7 = Conv1D(filter_nr, kernel_size=filter_size, padding='same', activation='linear', 
-                kernel_regularizer=conv_kern_reg, bias_regularizer=conv_bias_reg)(block6_output)
-    block7 = BatchNormalization()(block7)
-    block7 = PReLU()(block7)
-    block7 = Conv1D(filter_nr, kernel_size=filter_size, padding='same', activation='linear', 
-                kernel_regularizer=conv_kern_reg, bias_regularizer=conv_bias_reg)(block7)
-    block7 = BatchNormalization()(block7)
-    block7 = PReLU()(block7)
-
-    block7_output = add([block7, block6_output])
-    output = GlobalMaxPooling1D()(block7_output)
-
-    output = Dense(dense_nr, activation='linear')(output)
-    output = BatchNormalization()(output)
-    output = PReLU()(output)
-    output = Dropout(dense_dropout)(output)
-    output = Dense(5, activation='sigmoid')(output)
+        accuracy = sum([y_pred[i] == y_test[i] for i in range(len(y_pred))])/len(y_pred) * 100
+        print([y_pred[i] == y_test[i] for i in range(len(y_pred))])
+        print(accuracy, "%")
+        print(f1(y_pred, y_test))
     
-    # model = Model(comment, output)
-    # print("Correct model: ", type(model))
-    
-    model.compile(loss='binary_crossentropy', 
-            optimizer=optimizers.Adam(),
-            metrics=['accuracy'])
-    
-                
-    batch_size = 128
-    epochs = 4
-
-    Xtrain, Xval, ytrain, yval = train_test_split(x_train, y_train, train_size=0.95, random_state=233)
-    lr = callbacks.LearningRateScheduler(schedule)
-    ra_val = RocAucEvaluation(validation_data=(Xval, yval), interval = 1)
-    model.fit(Xtrain, ytrain, batch_size=batch_size, epochs=epochs, validation_data=(Xval, yval), callbacks = [lr, ra_val] ,verbose = 0)
-
-    
-    y_pred = model.predict(x_test)
-    y_pred = [[1 if i > 0.5 else 0 for i in r] for r in y_pred]
-    y_test = y_test.tolist()
-    accuracy = sum([y_pred[i] == y_test[i] for i in range(len(y_pred))])/len(y_pred) * 100
-    print([y_pred[i] == y_test[i] for i in range(len(y_pred))])
-    print(accuracy, "%")
-    """
-    submission = pd.read_csv('../input/jigsaw-toxic-comment-classification-challenge/sample_submission.csv')
-    submission[["toxic", "severe_toxic", "obscene", "threat", "insult", "identity_hate"]] = y_pred
-    submission.to_csv('dpcnn_test_preds.csv', index=False)
-    """
     return model
 
 # %% [code] {"scrolled:true"}
@@ -283,10 +171,10 @@ if __name__ == "__main__":
     global embedding_index
     start = 0
     emb_mean, emb_std, embeddings_index = extract_embed(EMBEDDING_FILE)
-    while(start<1):
-        model = load_model()
+    while(start<5):
+        model = 'ensemble_dpcnn_' + str(start) 
         model = classifier(model,emb_mean, emb_std, embeddings_index)
-        save_model(model)
+        #_save_model(model)
         start = start + 1
 
 
