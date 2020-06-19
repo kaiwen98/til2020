5e52c0b16180a22a9d5ed9ebe63df3f4f653202d
kaiwen98 <e0406836@u.nus.edu>
2020-06-19 20:15:18 +0800

    lol

--- nlp/stackensemble.py
+++ nlp/stackensemble.py
@@ -237,7 +237,7 @@ def classifier(model_name, emb_mean, emb_std, embeddings_index):
         batch_size = 128
         epochs = 25
         lr = callbacks.LearningRateScheduler(schedule)
-        ra_val = RocAucEvaluation(validation_data=(x_train[train], y_train[train]), interval = 1)
+        ra_val = RocAucEvaluation(validation_data=(inputX, y_train[train]), interval = 1)
         es = EarlyStopping(monitor = 'val_loss', verbose = 1, patience = 2, restore_best_weights = True, mode = 'min')
         mc = ModelCheckpoint(model_name, monitor='val_loss', mode='min', verbose=1, save_best_only= True, save_weights_only = True)
     
